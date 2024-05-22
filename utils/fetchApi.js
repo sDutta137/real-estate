32d00448b8f430bf5dfb3b004bb7d1f194af4ce1
victorcodesgmail <victoryentertain97@gmail.com>
2024-05-22 17:20:06 +0300

    pushed to github

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,8 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      // 'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': 'b272e49399msh51ef072b00247bdp17edcajsn5390ee72963e'
     },
   });
     
